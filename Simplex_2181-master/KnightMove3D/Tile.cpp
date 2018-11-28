9cacf4db4fde002b9b2f3faa25668c3e0d1d59d3
Sam <sxf5282@rit.edu>
2018-11-28 10:52:10 -0500

    Started Tile

--- Simplex_2181-master/KnightMove3D/Tile.cpp
+++ Simplex_2181-master/KnightMove3D/Tile.cpp
@@ -1,5 +1,5 @@
 #include "Tile.h"
-
+using namespace Simplex;
 
 
 Tile::Tile()
@@ -9,4 +9,26 @@ Tile::Tile()
 
 Tile::~Tile()
 {
+}
+
+void Simplex::Tile::Init()
+{
+}
+
+void Simplex::Tile::Step()
+{
+}
+
+void Simplex::Tile::GetMoves()
+{
+	int tempMoves[8][2] = { {position[0] + 1, position[1] + 2}, {position[0] + 2, position[1] + 1}, 
+			{position[0] + 2, position[1] - 1}, {position[0] + 1, position[1] - 2}, 
+			{position[0] - 1, position[1] - 2}, {position[0] - 2, position[1] + 1}, 
+			{position[0] - 2, position[1] + 1}, {position[0] - 1, position[1] + 2}};
+}
+
+bool Simplex::Tile::IsAlive()
+{
+	if (health > 0) return true;
+	else return false;
 }
--- Simplex_2181-master/KnightMove3D/Tile.h
+++ Simplex_2181-master/KnightMove3D/Tile.h
@@ -1,8 +1,26 @@
 #pragma once
-class Tile
+#include "Definitions.h"
+
+namespace Simplex
 {
-public:
-	Tile();
-	~Tile();
-};
+
+	class Tile
+	{
+	public:
+		Tile();
+		~Tile();
+		void Init();
+		void Step(); //Decrement health and change color
+		void GetMoves(); //Calculate and set possible moves
+		bool IsAlive(); //Returns true if health > 0
+		uint health;
+		int position[2]; //this tile's coordinates
+		int moves[8][2]; //array of possible moves
+
+		//Knight Stuff, whoops
+		//int origin[2]; //ij-coordinate of 
+		//int destination[2];
+	};
+
+}
 
