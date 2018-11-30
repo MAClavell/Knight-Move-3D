66ebec7c7b5907db2b30ffee850171412b191875
Sam <sxf5282@rit.edu>
2018-11-30 10:51:45 -0500

    LERPin

--- Simplex_2181-master/KnightMove3D/Knight.cpp
+++ Simplex_2181-master/KnightMove3D/Knight.cpp
@@ -3,18 +3,23 @@
 using namespace Simplex;
 
 //Constructor
-Knight::Knight(Simplex::String fileName, Simplex::String uniqueID, Board* brd) : Entity(fileName, uniqueID)
+Knight::Knight(Simplex::String fileName, Simplex::String uniqueID, Board* brd, SystemSingleton* a_system) : Entity(fileName, uniqueID)
 {
 	//Initialize entity manager and entity
 	entityMngr = EntityManager::GetInstance();
 	entityMngr->AddEntity(fileName, uniqueID);
 
-	//TO DO: initialize origin and destination tiles
+	system = a_system;
 
 	//Add board and set initial position
 	board = brd;
 	gridIndex = vector2(0, 0);
 	this->SetPosition(board->GetKnightPositionOnTile((int)gridIndex.x, (int)gridIndex.y));
+
+	//Initialize lerp values
+	origin = brd->GetTile(vector2(0, 0));
+	destination = brd->GetTile(vector2(1, 2));
+
 }
 
 //Descructor
@@ -66,23 +71,26 @@ void Knight::MoveKnight(int dir)
 
 void Knight::Jump()
 {
-	////get the percentage
-	//float fTimeBetweenStops = 2.0;//in seconds
-	////map the value to be between 0.0 and 1.0
-	//float fPercentage = MapValue(fTimer, 0.0f, fTimeBetweenStops, 0.0f, 1.0f);
 
-	////calculate the current position
-	//vector3 v3CurrentPos = glm::lerp(v3Start, v3End, fPercentage);
-	//float arc = sin(fPercentage * MATH_PI);
-	//v3CurrentPos.y += arc * maxHeight;
-	//matrix4 m4Model = glm::translate(IDENTITY_M4, v3CurrentPos);
+	static float fTimer = 0;//store the new timer
+	static uint uClock =system->GenClock(); //generate a new clock for that timer
+	fTimer += system->GetDeltaTime(uClock); //get the delta time for that timer
 
-	////if we are done with this route
-	//if (fPercentage >= 1.0f)
-	//{
-	//	fTimer = m_pSystem->GetDeltaTime(uClock);//restart the clock
-	//	Land(destination); 
-	//}
+	//map the percentage to be between 0.0 and 1.0
+	fPercentage = MapValue(fTimer, 0.0f, fTimeBetweenStops, 0.0f, 1.0f);
+
+	//calculate the current position
+	vector3 v3CurrentPos = glm::lerp(origin.GetKnightPosition(), destination.GetKnightPosition(), fPercentage);
+	float arc = sin(fPercentage * 3.14f);
+	v3CurrentPos.y += arc * maxHeight;
+	matrix4 m4Model = glm::translate(IDENTITY_M4, v3CurrentPos);
+
+	//if we are done with this route
+	if (fPercentage >= 1.0f)
+	{
+		fTimer = m_pSystem->GetDeltaTime(uClock);//restart the clock
+		Land(destination); 
+	}
 }
 
 void Knight::Land(Tile target)
@@ -107,7 +115,7 @@ void Knight::Land(Tile target)
 		vector2 move = target.GetMoves()[i];
 		if (move.x >= 0 && move.x < 8 && move.y >= 0 && move.y < 4)
 		{
-			//validMoves.push_back(board->GetTile(move));
+			validMoves.push_back(board->GetTile(move));
 		}
 	}
 
